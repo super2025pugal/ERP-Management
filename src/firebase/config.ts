c20c33463440405a70ca57b0025aa816488bbe88
Techie Pugal <pugalpugalee333@gmail.com>
2025-06-18 16:23:22 +0530

    make chnages

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,15 +1,16 @@
+// firebase.js or firebase.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSHZAKsRFrHer7lno6Rknr3w5j-xVglVc",
-  authDomain: "employeemanagement-16dba.firebaseapp.com",
-  projectId: "employeemanagement-16dba",
-  storageBucket: "employeemanagement-16dba.firebasestorage.app",
-  messagingSenderId: "723276151197",
-  appId: "1:723276151197:web:5df89d54869fc42dd4d774",
-  measurementId: "G-BT1YLST16T"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
